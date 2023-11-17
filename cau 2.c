cea2a67f0bff844201d27af81cac9d73c8b782ec
nqa1903 <103617099+nqa1903@users.noreply.github.com>
2023-11-17 09:44:29 +0700

    cau 2.c

--- cau 2.c
+++ cau 2.c
@@ -1,190 +1,171 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main()
-{
-	//nhap so phan tu mang hai chieu
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	//1. Khai bao mang so nguyen 2 chieu n*m
 	int n,m;
-	printf("Nhap so hang va so cot cua mang 2 chieu:");
-	scanf("%d%d",&m,&n);
-	//khai bao va nhap gia tri cho mang 2 chieu
-	int arr[m][n];
-	for(int i=0;i<m;i++)
-	{
-		for(int j=0;j<n;j++)
-		{
-			printf("Nhap arr[%d][%d]",i,j);
-			scanf("%d",&arr[i][j]);
-		}
-	}
-	//tao menu
-	do
-	{
-		printf("\n1.Nhap gia tri phan tu cua mang\n");
-		printf("2.In ra cac phan tu cua mang theo ma tran\n");
-		printf("3.In ra gia tri cac phan tu le va tinh tong\n");
-		printf("4.In ra gia tri phan tu nam tren duong bien va tinh tich\n");
-		printf("5.In ra cac phan tu tren duong cheo chinh va cheo phu va tinh tong\n");
-		printf("6.In gia tri nho nhat va lon nhat\n");
-		printf("7.In ra gia tri trung binh va so phan tu bang gia tri trung binh cua mang\n");
-		printf("8.In ra dong co tong gia tri phan tu la lon nhat");
-		printf("9.Thoat\n");
-		printf("Lua chon cua ban: \n");
-		//lua chon menu
-		int choice,sum,min,max,avg,count,row;
+	printf("Nhap vao so dong cua mang 2 chieu:");
+	scanf("%d",&n);
+	printf("Nhap vao so cot cua mang 2 chieu:");
+	scanf("%d",&m);
+	int numbers[n][m];
+	int sumNumbers;
+	int mulNumbers;
+	int min, max;
+	int avgValue;
+	int cntNumber;
+	int maxRow;
+	int sumRow;
+	int indexMaxRow;
+	//2. In menu
+	do{
+		printf("***************MENU**************\n");
+		printf("1. Nhap gia tri cac phan tu mang\n");
+		printf("2. In gia tri cac phan tu mang theo ma tran\n");
+		printf("3. In gia tri cac phan tu le va tinh tong\n");
+		printf("4. In gia tri cac phan tu tren duong bien va tinh tich\n");
+		printf("5. In gia tri cac phan tu cheo chinh, cheo phu va tinh tong\n");
+		printf("6. In gia tri lon nhat va nho nhat trong mang\n");
+		printf("7. Tinh gia tri trung binh va thong ke phan tu\n");
+		printf("8. In ra dong co tong gia tri phan tu lon nhat\n");
+		printf("9. Thoat\n");
+		printf("Lua chon cua ban:");
+		int choice;
 		scanf("%d",&choice);
-		switch(choice)
-		{
+		switch(choice){
 			case 1:
-				for(int i=0;i<m;i++)
-	            {
-		            for(int j=0;j<n;j++)
-		            {
-			            printf("Nhap arr[%d][%d]",i,j);
-			            scanf("%d",&arr[i][j]);
-		            }
-	            }
-	            break;
-	        case 2:
-	        	for(int i=0;i<m;i++)
-	        	{
-	        		for(int j=0;j<n;j++)
-	        		{
-	        			printf("%d\t",arr[i][j]);
+				printf("Nhap gia tri cac phan tu mang:\n");
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						printf("numbers[%d][%d]=",i,j);
+						scanf("%d",&numbers[i][j]);
+					}
+				}
+				break;
+			case 2:
+				printf("Gia tri cac phan tu trong mang:\n");
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						printf("%d\t",numbers[i][j]);
 					}
 					printf("\n");
 				}
+				printf("\n");
 				break;
 			case 3:
-				 sum=0;
-				for(int i=0;i<m;i++)
-				{
-					for(int j=0;j<n;j++)
-					{
-						if(arr[i][j]%2==1)
-						{
-							printf("%d/t",arr[i][j]);
-							sum+=arr[i][j];
+				sumNumbers=0;
+				printf("Cac phan tu le trong mang la:\n");
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						if(numbers[i][j]%2!=0){
+							printf("%d\t",numbers[i][j]);
+							sumNumbers+=numbers[i][j];
 						}
 					}
 				}
-				printf("Tong cac phan tu le la: %d",sum);
+				printf("\nTong cac phan tu le trong mang: %d\n",sumNumbers);
 				break;
 			case 4:
-				 sum=0;
-				for(int i=0;i<m;i++)
-				{
-					if(i==0||i==m-1)
-					{
-						for(int j=0;j<n;j++)
-					    {   
-						    if(j==0||j==n-1)
-						    {
-						    	sum+=arr[i][j];
-							}
-					    }
+				mulNumbers = 1;
+				printf("Cac phan tu nam tren duong bien:\n");
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						if(i==0 || i==n-1 || j ==0 || j == m-1){
+							printf("%d\t",numbers[i][j]);
+							mulNumbers*=numbers[i][j];
+						}
 					}
-					
 				}
-				sum=sum-arr[0][0]-arr[m-1][n-1]-arr[0][n-1]-arr[m-1][0];
-				printf("%d",sum);
+				printf("\nTich cac phan tu tren bien la: %d\n",mulNumbers);
 				break;
 			case 5:
-			    sum=0;
-			    if(m!=n)
-				{
-					printf("Khong co phan tu nao o duong cheo");
-				}
-				else
-				{
-					//duong cheo chinh
-					
-					sum=0;
-			        printf("Duong cheo chinh: ");
-			        for(int i=0;i<m;i++)
-			        {
-			    	printf("%d\t",arr[i][i]);
-			    	sum+=arr[i][i];
-				    }
-				    printf("Tong phan tu duong cheo chinh: %d",sum);
-				    //duong cheo phu
-				    sum=0;
-				    printf("Duong cheo phu:");
-				    int j=m-1;
-				    for(int i=0;i<m;i++)
-				    {
-				    	printf("%d",arr[i][j]);
-				    	sum+=arr[i][j];
-				    	j--;
+				if(n==m){
+					sumNumbers = 0;
+					printf("Cac phan tu nam tren duong cheo chinh:\n");
+					for(int i=0;i<n;i++){
+						for(int j=0;j<m;j++){
+							if(i==j){
+								printf("%d\t",numbers[i][j]);
+								sumNumbers+=numbers[i][j];
+							}
+						}
 					}
-					printf("Tong phan tu o duong cheo phu la: %d",sum);									    
-				}
+					printf("\nTong cac phan tu tren cheo chinh: %d\n",sumNumbers);
+					sumNumbers=0;
+					printf("Cac phan tu nam tren duong cheo phu:\n");
+					for(int i=0;i<n;i++){
+						for(int j=0;j<m;j++){
+							if(i+j==n-1){
+								printf("%d\t",numbers[i][j]);
+								sumNumbers+=numbers[i][j];
+							}
+						}
+					}
+					printf("\nTong cac phan tu tren cheo phu: %d\n",sumNumbers);
+				}else{
+					printf("Day khong phai ma tran vuong, khong co cheo chinh va cheo phu\n");	
+				}			
 				break;
 			case 6:
-			    min=arr[0][0];
-				max=arr[0][0];
-				for(int i=0;i<m;i++)
-				{
-					for(int j=0;j<n;j++)
-					{
-						if (min>arr[i][j])
-						{
-							min=arr[i][j];
+				min = numbers[0][0];
+				max = numbers[0][0];
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						if(min>numbers[i][j]){
+							min = numbers[i][j];
 						}
-						if(max<arr[i][j])
-						{
-							max=arr[i][j];
+						if(max<numbers[i][j]){
+							max = numbers[i][j];
 						}
 					}
 				}
-				printf("Gia tri nho nhat: %d.Gia tri lon nhat:%d",min,max);
+				printf("Gia tri lon nhat: %d - Nho nhat: %d\n",max,min);
 				break;
 			case 7:
-				sum=0;
-				count=0;
-				for(int i=0;i<m;i++)
-				{
-					for(int j=0;j<n;j++)
-					{
-						sum+=arr[i][j];
+				sumNumbers = 0;
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						sumNumbers+=numbers[i][j];
 					}
 				}
-				avg=sum/(m*n);
-				for(int i=0;i<m;i++)
-				{
-					for(int j=0;j<n;j++)
-					{
-						if(arr[i][j]==avg)
-						{
-							count+=1;
+				avgValue = sumNumbers/(n*m);
+				cntNumber = 0;
+				for(int i=0;i<n;i++){
+					for(int j=0;j<m;j++){
+						if(avgValue==numbers[i][j]){
+							cntNumber++;
 						}
 					}
 				}
-				printf("Gia tri trung binh cua mang la: %d,So phan tu bang gia tri trung binh: %d",avg,count);
+				printf("Co %d phan tu co gia tri bang gia tri trung binh: %d\n",cntNumber,avgValue);
 				break;
 			case 8:
-				row=0;
-				max=0;
-				for(int i=0;i<m;i++)
-				{
-					sum=0;
-					for(int j=0;j<n;j++)
-					{
-						sum+=arr[i][j];
-					}
-					if(sum>max)
-					{
-						max=sum;
-						row=i;
-					}					
+				//1. dat maxRow la dong 0
+				indexMaxRow = 0;
+				maxRow = 0;
+				for(int j=0;j<m;j++){
+					maxRow+=numbers[0][j];
 				}
-				printf("Dong co gia tri phan tu lon nhat la: %d",row);
+				//2. So sanh maxRow voi tong cac dong khac
+				for(int i=1;i<n;i++){
+					sumRow = 0;
+					for(int j=0;j<m;j++){
+						sumRow+= numbers[i][j];
+					}
+					if(maxRow<sumRow){
+						maxRow=sumRow;
+						indexMaxRow=i;
+					}
+				}
+				printf("Dong %d co tong cac phan tu lon nhat:\n",indexMaxRow);
+				for(int j=0;j<m;j++){
+					printf("%d\t",numbers[indexMaxRow][j]);
+				}
+				printf("\n");
 				break;
 			case 9:
 				exit(0);
 			default:
-				printf("Vui long chon tu 1 den 9");											 			    								
+				printf("Vui long nhap tu 1-9");
 		}
-	}
-	while(1==1);
+	}while(1==1);
 }
